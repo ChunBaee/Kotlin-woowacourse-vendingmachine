8aa1587a119f83b0df6e93b820b6f96c5aaa7dca
ChunBaee <mljung10@naver.com>
2022-12-03 17:14:14 +0900

    feat: 프로그램 실행 시 자판기 보유 금액을 입력받기 위한 안내 텍스트 출력

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,9 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    startProgram()
+}
+
+private fun startProgram() {
+    PrintForm().noticeInputVendingPrice()
 }
---
+++ src/main/kotlin/bridge/PrintForm.kt
@@ -0,0 +1,8 @@
+package bridge
+
+class PrintForm {
+
+    fun noticeInputVendingPrice() {
+        println("자판기가 보유하고 있는 금액을 입력해 주세요.")
+    }
+}
